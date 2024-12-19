9729e454422c9d67d61bfb878b7c01aa76fe411e
NoorN <NoorN@NOOR>
2024-12-19 14:13:41 +0300

    Add project files.

---
+++ Basics Part 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//3
+using System.Diagnostics;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("Numbers:" + i);
+}
+int countDown = 5;
+while(countDown > 0)
+{
+    Console.WriteLine(countDown);
+    countDown--;
+}
+
+//4
+string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday ", " Sunday", " Thursday" };
+for (  int i = 0;i < daysOfWeek.Length; i++)
+{
+    Console.WriteLine(daysOfWeek[i]);
+
+}
+
+// second bouns 
+// its useful when we want to make a table with row and colums and matrex
